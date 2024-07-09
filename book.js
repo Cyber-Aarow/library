2122d49a43c66f1e856b0ab820d5d611fa5b0f2c
Cyber-Aarow <136511597+Cyber-Aarow@users.noreply.github.com>
2024-07-09 16:40:55 -0700

    Add HTML and JS to display books on page.

--- book.js
+++ book.js
@@ -19,3 +19,30 @@ function addBookToLibrary(title, author, pages, read){
     let new_book = new Book(title, author, pages, read);
     myLibrary.push(new_book);
 }
+
+let library = document.querySelector('#library');
+for(i=0; i<library.length; i++){
+    let li = document.createElement('li');
+    let h4 = document.createElement('h4');
+    let div = document.createElement('div');
+    
+    h4.innerText = myLibrary[i].title;
+    li.appendChild(h4);
+    
+    div.innerText = myLibrary[i].author;
+    div.classList.add("author");
+    li.appendChild(div);
+    div.classList.remove("author");
+
+    div.innerText = myLibrary[i].pages;
+    div.classList.add("pages");
+    li.appendChild(div);
+    div.classList.remove("pages");
+
+    div.innerText = myLibrary[i].read;
+    div.classList.add("read");
+    li.appendChild(div);
+    div.classList.remove("read");
+
+    library.appendChild(li);
+}
