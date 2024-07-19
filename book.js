4388b99d007b029d18c234e93e0ca10969d3d2e6
Cyber-Aarow <136511597+Cyber-Aarow@users.noreply.github.com>
2024-07-19 11:46:56 -0700

    Try to get submit button working (fail) and change library display to be a function and to have a 'showing' boolean check to avoid reprints of books with each function call.

--- book.js
+++ book.js
@@ -1,10 +1,11 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read){
+function Book(title, author, pages, read, showing){
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.showing = showing;
 
     this.info = function(){
         let string = title + " by " + author + ", " + pages
@@ -15,63 +16,82 @@ function Book(title, author, pages, read){
     }
 }
 
-function addBookToLibrary(title, author, pages, read){
-    let new_book = new Book(title, author, pages, read);
+function addBookToLibrary(title, author, pages, read, showing){
+    let new_book = new Book(title, author, pages, read, showing);
     myLibrary.push(new_book);
 }
 
+function showLibrary(){
+    let library = document.querySelector('#library');
+    for(i=0; i<myLibrary.length; i++){
+        if(myLibrary[i].showing === false){
+            let li = document.createElement('li');
+            let title = document.createElement('h4');
+            let author = document.createElement('div');
+            let pages = document.createElement('div');
+            let read = document.createElement('div');
+            let title_and_author = document.createElement('div');
 
-//For Testing
-addBookToLibrary("LOTR", "Tolkien", 321, false);
-addBookToLibrary("Warriors", "Eoin Colfer", 123, true);
+            title.innerText = myLibrary[i].title;
+            title.classList.add("title");
+            title_and_author.appendChild(title);
+            
+            author.innerText = "By " + myLibrary[i].author;
+            author.classList.add("author");
+            title_and_author.appendChild(author);
 
+            //"Read" check at top
+            if(myLibrary[i].read){
+                read.innerText = "Have Read";
+                read.classList.add("read")
+            }
+            else {
+                read.innerText = "Not Read";
+                read.classList.add("not-read");
+            }
+            li.appendChild(read);
 
-//Set up library
-let library = document.querySelector('#library');
-for(i=0; i<myLibrary.length; i++){
-    let li = document.createElement('li');
-    let title = document.createElement('h4');
-    let author = document.createElement('div');
-    let pages = document.createElement('div');
-    let read = document.createElement('div');
-    let title_and_author = document.createElement('div');
+            //Then traditional book stuff
+            title_and_author.classList.add("title-and-author")
+            li.appendChild(title_and_author);
 
-    title.innerText = myLibrary[i].title;
-    title.classList.add("title");
-    title_and_author.appendChild(title);
-    
-    author.innerText = "By " + myLibrary[i].author;
-    author.classList.add("author");
-    title_and_author.appendChild(author);
+            //Pages at bottom
+            pages.innerText = "Pages: " + myLibrary[i].pages;
+            pages.classList.add("pages");
+            li.appendChild(pages);
 
-    //"Read" check at top
-    if(myLibrary[i].read){
-        read.innerText = "Have Read";
-        read.classList.add("read")
+            li.classList.add("book");
+            library.appendChild(li);
+            myLibrary[i].showing = true;
+        }
     }
-    else {
-        read.innerText = "Not Read";
-        read.classList.add("not-read");
-    }
-    li.appendChild(read);
-
-    //Then traditional book stuff
-    title_and_author.classList.add("title-and-author")
-    li.appendChild(title_and_author);
-
-    //Pages at bottom
-    pages.innerText = "Pages: " + myLibrary[i].pages;
-    pages.classList.add("pages");
-    li.appendChild(pages);
-
-    li.classList.add("book");
-    library.appendChild(li);
 }
 
-//Button
+//For Testing
+addBookToLibrary("LOTR", "Tolkien", 321, false, false);
+addBookToLibrary("Warriors", "Eoin Colfer", 123, true, false);
+
+showLibrary();
+
+
+//New Book Button
 let button = querySelector('#new-book');
 let form = document.querySelector('#add-book-form');
 button.addEventListener('click', function(){
-    form.classList.toggle('hidden');
-    form.classList.toggle('revealed');
+    form.classList.toggle('hide');
+    form.classList.toggle('reveal');
+});
+
+//Submit Form Button
+let submit = querySelector('#submit');
+submit.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    const new_title = document.querySelector('#form-title').value;
+    const new_author = document.querySelector('#form-author').value;
+    const new_pages = document.querySelector('#form-pages').value;
+    const new_read = document.querySelector('#read-check').value;
+
+    addBookToLibrary(new_title, new_author, new_pages, new_read);
+    showLibrary();
 });
